3ec34a235ab9c01a6693f4fc0db62d017f0d498e
LevanCode38 <LPNETINC2024@GMAIL.COM>
2025-02-12 22:23:44 -0500

    Create test_json.php

---
+++ test_json.php
@@ -0,0 +1,17 @@
+<?php
+// JSON ფაილის ჩატვირთვა
+$json_file = "products.json";
+
+// JSON მონაცემების წაკითხვა
+$json_data = json_decode(file_get_contents($json_file), true);
+
+// საძიებელი Product ID (მაგალითად, PayPal-დან მიღებული Product ID)
+$product_id = "course_123"; 
+
+// JSON-ში Product ID-ის მოძებნა
+if (isset($json_data[$product_id])) {
+    echo "✅ ნაპოვნია: " . $json_data[$product_id];
+} else {
+    echo "❌ ვერ მოიძებნა!";
+}
+?>
